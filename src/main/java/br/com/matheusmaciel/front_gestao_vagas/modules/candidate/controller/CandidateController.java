0ac0069894835267ed781090c22fab39eb652327
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2025-01-19 09:04:00 -0300

    Buscando dados do candidato na API

--- src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -3,6 +3,7 @@ package br.com.matheusmaciel.front_gestao_vagas.modules.candidate.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.matheusmaciel.front_gestao_vagas.modules.candidate.service.CandidateService;
+import br.com.matheusmaciel.front_gestao_vagas.modules.candidate.service.ProfileCandidateService;
 import jakarta.servlet.http.HttpSession;
 
 
@@ -22,6 +24,11 @@ public class CandidateController {
 
     @Autowired
     private CandidateService candidateService;
+
+    @Autowired
+    private ProfileCandidateService profileCandidateService;
+
+
     @GetMapping("/login")
     public String login() {
         return "candidate/login";
@@ -49,7 +56,9 @@ public class CandidateController {
 
     @GetMapping("/profile")
     @PreAuthorize("hasRole('CANDIDATE')")
-    public String profile(){
+    public String profile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        var result = this.profileCandidateService.execute(authentication.getDetails().toString());
         return "candidate/profile";
     }
 }
--- src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/candidate/service/CandidateService.java
+++ src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/candidate/service/CandidateService.java
@@ -11,15 +11,19 @@ import br.com.matheusmaciel.front_gestao_vagas.modules.candidate.dto.Token;
 @Service
 public class CandidateService {
     
-    public Token login(String username, String password){
+    public Token login(String username, String password) {
         RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         Map<String, String> data = new HashMap<>();
         data.put("username", username);
         data.put("password", password);
-        HttpEntity<Map<String,String>> request = new HttpEntity<>(data, headers);
+        
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(data, headers);
+
         var result = rt.postForObject("http://localhost:8080/candidate/auth", request, Token.class);
+
+        System.out.println(result);
         
         return result;
     }
