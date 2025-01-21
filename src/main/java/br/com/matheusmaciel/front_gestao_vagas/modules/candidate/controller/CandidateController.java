207f325dc76150d2fb43e6a4fa47e1667ffe1f7f
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2025-01-21 18:50:10 -0300

    Tratando Login Expirado
    Signed-off-by: srmatheusmaciel <srmatheusmaciel@hotmail.com>

--- src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -58,9 +58,18 @@ public class CandidateController {
     @GetMapping("/profile")
     @PreAuthorize("hasRole('CANDIDATE')")
     public String profile(Model model) {
+
+        try{
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         var user = this.profileCandidateService.execute(authentication.getDetails().toString());
+
         model.addAttribute("user", user);
+
         return "candidate/profile";
+
+        }catch (HttpClientErrorException e) {
+            return "redirect:/candidate/login";
+
+        }
     }
 }
--- src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
+++ src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
@@ -4,8 +4,11 @@ import java.util.Map;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 
 import br.com.matheusmaciel.front_gestao_vagas.modules.candidate.dto.ProfileUserDTO;
 @Service
@@ -15,8 +18,15 @@ public class ProfileCandidateService {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
         HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
+
+        try{
         var result = rt.exchange("http://localhost:8080/candidate/", HttpMethod.GET, request, ProfileUserDTO.class);
         System.out.println(result);
+
         return result.getBody();
+        
+        }catch (Unauthorized e) {
+            throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
