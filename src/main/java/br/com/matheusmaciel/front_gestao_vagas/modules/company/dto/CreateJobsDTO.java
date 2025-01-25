d94ff411eb8b8d25608fc1ac2efdc7c395c847e2
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2025-01-25 05:34:00 -0300

    Criando cadastro de vagas

---
+++ src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/company/dto/CreateJobsDTO.java
@@ -0,0 +1,10 @@
+package br.com.matheusmaciel.front_gestao_vagas.modules.company.dto;
+
+import lombok.Data;
+@Data
+public class CreateJobsDTO {
+    
+    private String description;
+    private String benefits;
+    private String level;
+}
