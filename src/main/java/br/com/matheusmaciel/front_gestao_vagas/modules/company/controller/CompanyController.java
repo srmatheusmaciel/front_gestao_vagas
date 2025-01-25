08b83745af6c0d6a35a38638703a9bfd0e536d33
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2025-01-25 06:59:33 -0300

    logout company

--- src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/company/controller/CompanyController.java
+++ src/main/java/br/com/matheusmaciel/front_gestao_vagas/modules/company/controller/CompanyController.java
@@ -106,6 +106,16 @@ public class CompanyController {
         return "company/list";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        SecurityContextHolder.getContext().setAuthentication(null);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+        session.setAttribute("token", null);
+        return "redirect:/company/login";
+    }
+
+
 
 
     private String getToken(){
